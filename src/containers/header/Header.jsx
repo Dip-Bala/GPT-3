52a1c15b9fdc30f881880911a6fad377d608922f
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-04-04 02:50:54 +0530

    Complete Header Section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,26 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="gpt3-header section-padding">
+      <div className="gpt3-header-content">
+        <h1 className="gradient-text ">Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+        <div className="gpt3-header-content-input">
+          <input type="email" placeholder="Your Email Address" />
+          <button tupe = "button">Get Started</button>
+        </div>
+
+        <div className="gpt3-header-content-people">
+          <img src="../../assets/people.png" alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        </div>
+        <div className="gpt3-header-image">
+          <img src="../../assets/ai.png" alt="ai" />
+        </div>
+      
+    </div>
   )
 }
 
