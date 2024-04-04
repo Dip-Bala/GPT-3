ea3c3e28b1d140bcc706bcae004536bcb5c456db
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-04-04 10:40:51 +0530

    Update package.json and complete Brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -4,7 +4,15 @@ import './Brand.css';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className = "gpt3-brand">
+      <div className = "gpt3-brand-content">
+        <img src="../../assets/google.png" alt="Google" />
+        <img src="../../assets/slack.png" alt="slack" />
+        <img src="../../assets/atlassian.png" alt="atlassian" />
+        <img src="../../assets/dropbox.png" alt="dropbox" />
+        <img src="../../assets/shopify.png" alt="shopify" />
+      </div>
+    </div>
   )
 }
 
