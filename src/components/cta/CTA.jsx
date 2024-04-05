ea537599aa411b0a00f74ef4b2a0a4983328ee4f
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-04-05 07:20:13 +0530

    Complete CTA section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -4,7 +4,15 @@ import './CTA.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className = "gpt3-cta">
+      <div className = "gpt3-cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <div className = "gpt3-cta-button">
+        <button>Get Started</button>
+      </div>
+    </div>
   )
 }
 
