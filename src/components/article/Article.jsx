490e37d4f41520db1283a77d8ee4d27ab4ec732e
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-04-05 15:42:30 +0530

    Complete Blog section

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './Article.css';
-const Article = () => {
+const Article = (props) => {
   return (
-    <div>Article</div>
+    <div className="gpt3-article">
+      <div className="gpt3-article-image">
+        <img src={props.imgURL} alt="blog-image" />
+      </div>
+      <div className="gpt3-article-content">
+        <div>
+          <p>{props.date}</p>
+          <h3>{props.text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './Blog.css';
+import Article from '../../components/article/Article';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div  className="gpt3-blog section-margin">
+      <div className="gpt3-blog-heading">
+        <h1 className = "gradient-text">A lot is happening, <br></br>
+        We are blogging about it.</h1>
+      </div>
+      <div className="gpt3-blog-Articles">
+        <div className="gpt3-blog-Articles-left">
+          <Article imgURL = "../../assets/blog01.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+        <div className="gpt3-blog-Articles-right">
+        <Article imgURL=  "../../assets/blog02.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgURL=  "../../assets/blog03.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgURL=  "../../assets/blog04.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgURL=  "../../assets/blog05.png" date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+      </div>
+    </div>
   )
 }
 
