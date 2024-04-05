48577a76f8ab42e32bc4f68b6b20719dc043a2c4
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-04-05 06:26:43 +0530

    Complete possibility section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import {Article, Brand, CTA, Feature, Navbar} from "./components";
-import {Footer, Header, WhatGPT3, Possibility, Blog, AppFeature} from "./containers";
+import { Brand, CTA, Navbar } from "./components";
+import { Footer, Header, WhatGPT3, Possibility, Blog, AppFeature } from "./containers";
 import "./App.css";
 const App = () => {
   return (
-    <div className = "App">
-      <div className = "gradient-bg">
+    <div className="App">
+      <div className="gradient-bg">
         <Navbar />
         <Header />
-
       </div>
       <Brand />
       <WhatGPT3 />
       <AppFeature />
       <Possibility />
-      <CTA/>
+      <CTA />
       <Blog />
-      <Footer/>
+      <Footer />
     </div>
   )
 }
